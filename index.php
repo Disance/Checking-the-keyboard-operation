e52d6a592ec0c546d2b1a94754ba4c156a03cf1f
Demochkin Roman <foxheppi319@gmail.com>
2024-02-06 22:08:58 +0600

    Push

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Type</title>
+</head>
+<body>
+    <div class="container">
+        <div class="keyBoard">
+            <div class="key q">Q</div>
+            <div class="key w">W</div>
+            <div class="key e">E</div>
+            <div class="key r">R</div>
+            <div class="key t">T</div>
+            <div class="key y">Y</div>
+            <div class="key u">U</div>
+            <div class="key i">I</div>
+            <div class="key o">O</div>
+            <div class="key p">P</div><br>
+            <div class="key a">A</div>
+            <div class="key s">S</div>
+            <div class="key d">D</div>
+            <div class="key f">F</div>
+            <div class="key g">G</div>
+            <div class="key h">H</div>
+            <div class="key j">J</div>
+            <div class="key k">K</div>
+            <div class="key l">L</div><br>
+            <div class="key z">Z</div>
+            <div class="key x">X</div>
+            <div class="key c">C</div>
+            <div class="key v">V</div>
+            <div class="key b">B</div>
+            <div class="key n">N</div>
+            <div class="key m">M</div><br>
+        </div>
+        <div class="subkey">
+        <div class="key shift">SHIFT</div>
+        <div class="key ctrl">CTRL</div>
+        <div class="key alt">ALT</div>
+        <div class="key space">Space</div>
+    </div>
+    </div>
+</body>
+</html>
